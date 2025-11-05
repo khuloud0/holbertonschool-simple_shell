33ca0bb6922c05eae8ddfdc9ff11c88f28c9ca71
root <root@ip-10-42-14-199.ec2.internal>
2025-11-05 10:26:49 +0000

    add1

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+char **parse_line(char *line);
+void free_tokens(char **tokens);
+char *find_path(char *command);
+int _strlen(const char *s);
+
+#endif
