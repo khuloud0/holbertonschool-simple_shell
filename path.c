5547b112c9c7097dd59fcf5b773602695997da89
AleidBanan <Aleidbanan@gmail.com>
2025-11-09 13:26:35 +0000

    fix documentation task 1

--- path.c
+++ path.c
@@ -2,10 +2,31 @@
 #include <sys/stat.h>
 
 /**
- * find_path - Finds the full path of a command using PATH
- * @command: The command to search for
+ *	check_absolute - Checks if the given command is an absolute or relative path
+ *	@command: The command string to check
  *
- * Return: Full path string (malloc'd), or NULL if not found
+ *	Return: A duplicated string of the command if it exists, NULL otherwise.
+ */
+
+/**
+ *	get_path_env - Retrieves the PATH variable from the environment
+ *
+ *	Return: Pointer to the PATH value if found, or NULL otherwise.
+ */
+
+/**
+ *	search_in_path - Searches directories listed in PATH for an executable
+ *	@path_copy: A duplicated PATH string (will be tokenized)
+ *	@command: The command name to search for
+ *
+ *	Return: malloc'd full path string if found, or NULL otherwise.
+ */
+
+/**
+ *	find_path - Finds the full path of a command using PATH
+ *	@command: The command to search for
+ *
+ *	Return: Full path string (malloc'd), or NULL if not found
  */
 char *find_path(char *command)
 {
@@ -23,7 +44,6 @@ char *find_path(char *command)
 		else
 			return (NULL);
 	}
-
 	while (environ[i])
 	{
 		if (strncmp(environ[i], "PATH=", 5) == 0)
