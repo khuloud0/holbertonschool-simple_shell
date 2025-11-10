97e44d95ae8c3967face0cc7a2c9f67f55e42e52
root <root@ip-10-42-128-185.ec2.internal>
2025-11-10 06:25:09 +0000

    fix1

--- main.c
+++ main.c
@@ -1,107 +1,94 @@
-#include "Shell.h"
+#include "shell.h"
 
 /**
- * print_prompt - Prints the shell prompt if in interactive mode
- */
-void print_prompt(void)
-{
-	if (isatty(STDIN_FILENO))
-		write(STDOUT_FILENO, "#cisfun$ ", 9);
-}
-
-/**
- * handle_input - Reads a line of input from stdin and removes trailing newline
- *
- * Return: Pointer to the input string, or NULL on EOF/error
- */
-char *handle_input(void)
-{
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-
-	read = getline(&line, &len, stdin);
-	if (read == -1)
-	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "\n", 1);
-		return (NULL);
-	}
-
-	if (line[read - 1] == '\n')
-		line[read - 1] = '\0';
-
-	return (line);
-}
-
-/**
- * handle_builtins - Handles built-in shell commands like exit and env
- * @args: Array of arguments
- * @argv: Program name and arguments from main
- *
- * Return: 0 if a builtin command was executed, -1 otherwise
- */
-int handle_builtins(char **args, char **argv)
-{
-	int i;
-
-	if (strcmp(args[0], "exit") == 0)
-		exit(0);
-
-	if (strcmp(args[0], "env") == 0)
-	{
-		for (i = 0; environ[i]; i++)
-		{
-			write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
-			write(STDOUT_FILENO, "\n", 1);
-		}
-		return (0);
-	}
-
-	return (-1);
-}
-
-/**
- * main - Main loop of the shell
+ * main - Entry point for the shell
  * @argc: Argument count
  * @argv: Argument vector
  *
- * Return: Exit status of the last executed command
+ * Return: Exit status of the last command
  */
 int main(int argc, char **argv)
 {
-	char *line, **args;
-	int status = 0;
+	char *line = NULL, *full_cmd = NULL;
+	size_t len = 0;
+	ssize_t read;
+	char **args;
+	int status = 0, i;
 
 	(void)argc;
 
 	while (1)
 	{
-		print_prompt();
-		line = handle_input();
-		if (!line)
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "#cisfun$ ", 9);
+
+		read = getline(&line, &len, stdin);
+		if (read == -1)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
 			break;
+		}
+
+		if (line[read - 1] == '\n')
+			line[read - 1] = '\0';
 
 		args = parse_line(line);
-		if (!args || !args[0])
+		if (args == NULL || args[0] == NULL)
 		{
 			free_tokens(args);
-			free(line);
 			continue;
 		}
 
-		if (handle_builtins(args, argv) == 0)
+		if (strcmp(args[0], "exit") == 0)
 		{
 			free_tokens(args);
 			free(line);
+			exit(status);
+		}
+
+		if (strcmp(args[0], "env") == 0)
+		{
+			for (i = 0; environ[i]; i++)
+			{
+				write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
+				write(STDOUT_FILENO, "\n", 1);
+			}
+			free_tokens(args);
 			continue;
 		}
 
-		status = execute_command(args, argv);
+		full_cmd = find_path(args[0]);
+		if (full_cmd == NULL)
+		{
+			fprintf(stderr, "%s: 1: %s: not found\n", argv[0], args[0]);
+			free_tokens(args);
+			status = 127;
+			continue;
+		}
 
+		if (fork() == 0)
+		{
+			if (execve(full_cmd, args, environ) == -1)
+			{
+				perror(argv[0]);
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			wait(&status);
+			if (WIFEXITED(status))
+				status = WEXITSTATUS(status);
+			else
+				status = 2;
+		}
+
+		free(full_cmd);
 		free_tokens(args);
-		free(line);
 	}
 
+	free(line);
 	return (status);
 }
+
